94b3c27db7c3d1556cbfd6f3f21ea60a6107b36a
oliviablank10 <125896715+oliviablank10@users.noreply.github.com>
2023-03-06 14:46:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const url = "https://icanhazdadjoke.com/j/R7UfaahVfFd"
+async function getData() {
+	const response = await fetch(url)
+	const data = await response.json()
+	console.log(data.joke)
+	//console.log(data.character.name)
+
+	document.getElementById("joke").textContent = "'" + data.sentence + "'"
+	//document.getElementById("person").textContent = "-" + data.character.name
+}
+
+var view=document.getElementById("view");
+view.addEventListener("click", getData, false);
+
