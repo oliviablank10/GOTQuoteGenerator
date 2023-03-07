e12b18c9f0e31c8b6334ecd819429459548c91dc
oliviablank10 <125896715+oliviablank10@users.noreply.github.com>
2023-03-06 18:29:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const url = "https://api.gameofthronesquotes.xyz/v1/random"
 async function getData() {
 	const response = await fetch(url)
 	const data = await response.json()
-	console.log(data.joke)
+	console.log(data.sentence)
 	console.log(data.character.name)
 
-	document.getElementById("joke").textContent = "'" + data.sentence + "'"
+	document.getElementById("sentence").textContent = "'" + data.sentence + "'"
 	document.getElementById("person").textContent = "-" + data.character.name
 }
 
